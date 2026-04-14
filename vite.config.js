9b143cebf787218c39f393825e2269dfa13c4103
Raul andres Gutierrez <raulandresgutierrez@MacBook-Air-de-Raul.local>
2026-04-14 15:34:51 -0300

    ci: GitHub Actions deploy workflow + vite base para /kiosco/

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kiosco/',   // subdirectorio en GitHub Pages / event-pix.com.ar/kiosco
 })
